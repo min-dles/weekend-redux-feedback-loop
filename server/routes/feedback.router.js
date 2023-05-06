a24f5cd8c284f0dbddc272c5218df8ee7174cdef
mandi andrade <mandi.andrade20@gmail.com>
2023-05-06 16:35:46 -0500

    started to build server router and routes

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool.js');
+
+// STRETCH GOAL - will need to show all DB info 
+// ordered by date (newest entries at top of table)
+router.get('/admin', (req, res) => {
+    const sqlText = `
+        SELECT * FROM feedback
+            ORDER BY date DESC;
+    `;
+    pool.query(sqlText)
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('GET /admin fail:', error);
+            res.sendStatus(500);
+        })
+})
+
+// BASE MODE: need final object from review page entered
+// to the DB "feedback" table
+router.post('/review', (req, res) => {
+    const sqlText = `
+        INSERT INTO feedback
+            (feeling, understanding, support, comments)
+            VALUES
+            ($1, $2, $3, $4)
+    `;
+
+    const sqlValues = [
+        req.body.feeling, 
+        req.body.understanding, 
+        req.body.support, 
+        req.body.comments
+    ]
+
+    pool.query(sqlText, sqlValues)
+        .then((result) => {
+            res.sendStatus(201);
+            console.log('Thanks for submitting your feedback!');
+        })
+        .catch((error) => {
+            console.log('POST /review fail:', error);
+            res.sendStatus(500);
+        })
+})
+
+module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import React from 'react';
 import axios from 'axios';
 import './App.css';
@@ -6,10 +7,23 @@ function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
+      <Router>  
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/feeling"></Route>
+        <Route exact path="/understanding"></Route>
+        <Route exact path="/support"></Route>
+        <Route exact path="comments"></Route>
+        <Route exact path="/review">
+          <Review />
+        </Route>
+        <Route exact path="/admin"></Route>
+      </Router>
     </div>
   );
 }
